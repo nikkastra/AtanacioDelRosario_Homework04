9e01ed32c4ea3aa3f96448a3318764bdf4cdb151
leftfordead999 <franz.atanacio@obf.ateneo.edu>
2024-04-05 03:06:40 +0800

    may tileset code na, magdodrawing na lang

--- Main.cpp
+++ Main.cpp
@@ -11,6 +11,14 @@
 const int WINDOW_WIDTH = 1280;
 const int WINDOW_HEIGHT = 720;
 const float FPS = 60;
+const int PIXELS = 128;
+
+struct Tile{
+    Rectangle locationInSpriteSheet;
+    bool has_collision;
+};
+
+Tile* allTiles = new Tile[2];
 
 int main(){
 
@@ -36,7 +44,45 @@ int main(){
     camera_view.offset = {WINDOW_WIDTH/2, WINDOW_HEIGHT/2};
     camera_view.zoom = 1.0f;
 
-    Texture joshHutcherson = LoadTexture("josh_hutcherson.png");
+    // Texture joshHutcherson = LoadTexture("josh_hutcherson.png");
+
+    //--------------DRAW TILESET ALGORITHM----------------//
+    char tilesetName[20];
+    int tileCount;
+    int tileIndex = 0;
+    int gridSizeX = -1;
+    int gridSizeY = -1;
+    std::vector<std::vector<int>> grid;
+
+    FILE* configFile = fopen("tileset.txt", "r");
+    if (configFile) {
+        fscanf(configFile, "IMAGE_NAME %s\n", &tilesetName);
+        fscanf(configFile, "TILE_COUNT %i\n", &tileCount);
+        int h,w,x,y;
+        for(int i = 0; i < tileCount; i++){
+            fscanf(configFile, "%i %i %i %i\n", &x, &y, &w, &h);
+            allTiles[i].locationInSpriteSheet.x = x;
+            allTiles[i].locationInSpriteSheet.y = y;
+            allTiles[i].locationInSpriteSheet.width = w;
+            allTiles[i].locationInSpriteSheet.height = h;
+        }
+        fscanf(configFile, "GRID %i %i\n", &gridSizeX, &gridSizeY);
+        for(int i = 0; i < gridSizeY; i++){
+            std::vector<int> row;
+            for(int j = 0; j < gridSizeX; j++){
+                int g;
+                fscanf(configFile, "%i", &g);
+                row.push_back(g);
+            }
+            grid.push_back(row);
+        }
+        fclose(configFile);
+        std::cout << "Config file SUCCESS\n";
+    } else {
+    std::cerr << "Error";
+    }
+
+    Texture tileMap = LoadTexture(tilesetName);
 
     while(!WindowShouldClose()){
         float delta_time = GetFrameTime();
@@ -100,7 +146,13 @@ int main(){
         BeginDrawing();
         ClearBackground(WHITE);
         BeginMode2D(camera_view);
-        DrawTexture(joshHutcherson, 0, 0, WHITE);
+        // DrawTexture(joshHutcherson, 0, 0, WHITE);
+        for(int i = 0; i < grid.size(); i++){
+            std::vector<int> row = grid[i];
+            for(int j = 0; j < row.size(); j++){
+                DrawTexturePro(tileMap, allTiles[row[j]].locationInSpriteSheet, {(float) (PIXELS*j), (float) (PIXELS*i), PIXELS, PIXELS}, Vector2Zero(), 0, WHITE);
+            }
+        }
         
         if(enemy1._healthPoints > 0 ) enemy1.Draw();
         if(enemy2._healthPoints  > 0) enemy2.Draw();
