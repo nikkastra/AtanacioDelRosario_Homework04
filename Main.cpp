9e026989259cd8e46485c1a24003c1479024a0be
leftfordead999 <franz.atanacio@obf.ateneo.edu>
2024-04-09 21:20:01 +0800

    eto pala yun dapat haha

--- Main.cpp
+++ Main.cpp
@@ -11,7 +11,7 @@
 const int WINDOW_WIDTH = 1280;
 const int WINDOW_HEIGHT = 720;
 const float FPS = 60;
-const int PIXELS = 128;
+const int PIXELS = 16;
 
 struct Tile{
     Rectangle locationInSpriteSheet;
@@ -21,21 +21,6 @@ struct Tile{
 Tile* allTiles = new Tile[2];
 
 int main(){
-
-    //full entity parameter: 
-    //Entity(Vector2 pos = {0,0}, float radius = 0.0f, float size = 0.0f, float speed = 0, float healthPoints = 0);
-    // note that the equal signs are default values
-    Player player({WINDOW_WIDTH/2, WINDOW_HEIGHT/2}, 50.0f, 200.0f, 5); // for player Player(Vector2 pos, float rad, float spd, int hp);
-    Enemy enemy1({WINDOW_WIDTH/8, WINDOW_HEIGHT/8}, 100.0f, 100.0f, 2); // for enemy Vector2 pos, float sz, float spd, int hp
-    Enemy enemy2({7*WINDOW_WIDTH/8, 7*WINDOW_HEIGHT/8}, 100.0f, 100.0f, 2); 
-
-    Item item1 ({0, 0}, 31,2,1);
-    Item item2 ({WINDOW_WIDTH/2 + 400, WINDOW_HEIGHT/9 + 200}, 31,2,1);
-    Item item3 ({WINDOW_WIDTH/9 + 150, WINDOW_HEIGHT/2 + 200}, 31,2,1);
-    Item item4 ({WINDOW_WIDTH/2 + 1000, WINDOW_HEIGHT + 200}, 31,2,1);
-    Item item5 ({WINDOW_WIDTH, WINDOW_HEIGHT}, 31,2,1);
-
-
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "miss ko na siya");
     SetTargetFPS(FPS);
 
@@ -52,11 +37,24 @@ int main(){
     int tileIndex = 0;
     int gridSizeX = -1;
     int gridSizeY = -1;
+    Vector2 playerPos;
+    Vector2 enemyOne;
+    Vector2 enemyTwo;
+    Vector2 enemyThree;
     std::vector<std::vector<int>> grid;
 
-    FILE* configFile = fopen("tileset.txt", "r");
+    FILE* configFile = fopen("level1.txt", "r");
     if (configFile) {
         fscanf(configFile, "IMAGE_NAME %s\n", &tilesetName);
+        int px, py, e1x, e1y, e2x, e2y, e3x, e3y;
+        fscanf(configFile, "PLAYER_POS %i %i\n", &px, &py);
+        playerPos = {(float) px, (float) py};
+        fscanf(configFile, "ENEMY_ONE_POS %i %i\n", &e1x, &e1y);
+        enemyOne = {(float) e1x, (float) e1y};
+        fscanf(configFile, "ENEMY_TWO_POS %i %i\n", &e2x, &e2y);
+        enemyTwo = {(float) e2x, (float) e2y};
+        fscanf(configFile, "ENEMY_THREE_POS %i %i\n", &e3x, &e3y);
+        enemyThree = {(float) e3x, (float) e3y};
         fscanf(configFile, "TILE_COUNT %i\n", &tileCount);
         int h,w,x,y;
         for(int i = 0; i < tileCount; i++){
@@ -82,6 +80,21 @@ int main(){
     std::cerr << "Error";
     }
 
+    //full entity parameter: 
+    //Entity(Vector2 pos = {0,0}, float radius = 0.0f, float size = 0.0f, float speed = 0, float healthPoints = 0);
+    // note that the equal signs are default values
+    Player player(playerPos, 50.0f, 200.0f, 5); // for player Player(Vector2 pos, float rad, float spd, int hp);
+    Enemy enemy1(enemyOne, 100.0f, 100.0f, 2); // for enemy Vector2 pos, float sz, float spd, int hp
+    Enemy enemy2(enemyTwo, 100.0f, 100.0f, 2);
+    Enemy enemy3(enemyThree, 100.0f, 100.0f, 2);
+    std::cout << enemy3._position.x << "|" << enemy3._position.y << std::endl;
+
+    Item item1 ({0, 0}, 31,2,1);
+    Item item2 ({WINDOW_WIDTH/2 + 400, WINDOW_HEIGHT/9 + 200}, 31,2,1);
+    Item item3 ({WINDOW_WIDTH/9 + 150, WINDOW_HEIGHT/2 + 200}, 31,2,1);
+    Item item4 ({WINDOW_WIDTH/2 + 1000, WINDOW_HEIGHT + 200}, 31,2,1);
+    Item item5 ({WINDOW_WIDTH, WINDOW_HEIGHT}, 31,2,1);
+
     Texture tileMap = LoadTexture(tilesetName);
 
     while(!WindowShouldClose()){
@@ -93,14 +106,26 @@ int main(){
         if(enemy1._healthPoints > 0){
             player.HandleCollision(&enemy1);
             enemy1.Update(delta_time);
+
             enemy1.HandleCollision(&player);
             enemy2.HandleEnemyCollision(&enemy1);
+            enemy3.HandleEnemyCollision(&enemy1);
         }
         if(enemy2._healthPoints > 0){
             player.HandleCollision(&enemy2);
             enemy2.Update(delta_time);
+
             enemy2.HandleCollision(&player);
             enemy1.HandleEnemyCollision(&enemy2);
+            enemy3.HandleEnemyCollision(&enemy2);
+        }
+        if(enemy3._healthPoints > 0){
+            player.HandleCollision(&enemy2);
+            enemy3.Update(delta_time);
+
+            enemy3.HandleCollision(&player);
+            enemy1.HandleEnemyCollision(&enemy3);
+            enemy2.HandleEnemyCollision(&enemy3);
         }
 
         if(item1._healthPoints > 0)
@@ -146,7 +171,6 @@ int main(){
         BeginDrawing();
         ClearBackground(WHITE);
         BeginMode2D(camera_view);
-        // DrawTexture(joshHutcherson, 0, 0, WHITE);
         for(int i = 0; i < grid.size(); i++){
             std::vector<int> row = grid[i];
             for(int j = 0; j < row.size(); j++){
@@ -156,6 +180,7 @@ int main(){
         
         if(enemy1._healthPoints > 0 ) enemy1.Draw();
         if(enemy2._healthPoints  > 0) enemy2.Draw();
+        if(enemy3._healthPoints  > 0) enemy3.Draw();
         
         if(item1._healthPoints > 0) item1.Draw();
         if(item2._healthPoints > 0) item2.Draw();
@@ -171,7 +196,7 @@ int main(){
         sprintf_s(buffer, "%.0i", player._healthPoints);
         DrawText(buffer, 10, 10, 100, BLACK);
         // win condition
-        if(enemy1._healthPoints <= 0 && enemy2._healthPoints <=0)
+        if(enemy1._healthPoints <= 0 && enemy2._healthPoints <=0 && enemy3._healthPoints <=0)
         {
             DrawRectangle(0,0,WINDOW_WIDTH,WINDOW_HEIGHT,GRAY);
             DrawText("You Win!", (WINDOW_WIDTH/2) - 175,(WINDOW_HEIGHT/2) - 50,100,GREEN);
